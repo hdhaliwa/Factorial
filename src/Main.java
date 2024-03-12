1eb6a042b134e34374ef226ae252136c15ac90c8
hdhaliwa <hdhaliwa@asu.edu>
2024-03-12 09:48:48 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "*";
+        int num = 5;
+        //find factorial of the number 10
+        for(int i = 1; i<num ; i++) {
+            for(int j = 0; j<i; j++) {
+                System.out.print(a);
+            }
+            System.out.println();
+        }
+
+    }
+}
